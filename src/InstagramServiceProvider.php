3b31e9868c31e3fecf15cf233a7d47ae2f81774a
Sang Huỳnh <giacatluong.it@gmail.com>
2020-12-15 19:12:06 +0700

    init

---
+++ src/InstagramServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace HMS92\Laravel\OAuth2\Instagram;
+
+use Illuminate\Support\ServiceProvider;
+
+class InstagramServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
